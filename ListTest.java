e3e507b2d797438a1abbd7386b787b71d83fc5be
Anurag Anand <anuraganand789@hotmail.com>
2020-10-10 14:40:55 +0530

    use forEach function of list to print elements

--- ListTest.java
+++ ListTest.java
@@ -7,6 +7,8 @@ public class ListTest {
 	//Create an immutable list
         final List<String> listImmutable = List.of("Anurag", "Anand");
 	listImmutable.stream().forEach(System.out::println);
+	//using list's own forEach function
+	listImmutable.forEach(System.out::println);
     }
 
 }
