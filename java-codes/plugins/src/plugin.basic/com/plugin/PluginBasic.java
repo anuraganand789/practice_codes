9db0320061825ef10bdc37dd163f86267dfdcfc5
Anurag Anand <anuraganand789@hotmail.com>
2021-03-14 23:54:06 +0530

    an attempt to make Plugin interface work

---
+++ java-codes/plugins/src/plugin.basic/com/plugin/PluginBasic.java
@@ -0,0 +1,39 @@
+package com.plugin;
+
+import com.sun.source.util.Plugin;
+import com.sun.source.util.JavacTask;
+import com.sun.source.util.TaskListener;
+import com.sun.source.util.TaskEvent;
+
+import static com.sun.source.util.TaskEvent.Kind;
+
+public class PluginBasic implements Plugin {
+   
+    public static void main(final String... args){
+        System.out.println(new PluginBasic().getName());
+    }
+
+    @Override
+    public String getName() { return this.class.getName() ; } 
+
+    @Override
+    public void init(final JavacTask javaCTask, String ... args){
+        final JavacTaskListener javacTaskListener = this.new JavacTaskListener();
+        javaCTask.addTaskListener(javacTaskListener);
+    }
+
+    private class JavacTaskListener implements TaskListener{
+
+        @Override
+        public void finished(final TaskEvent taskEvent){
+           System.out.println("Finished " + taskEvent.getKind());
+        }
+
+        @Override
+        public void started(final TaskEvent taskEvent){
+           System.out.println("Started " + taskEvent.getKind());
+        }
+
+    }
+}
+
