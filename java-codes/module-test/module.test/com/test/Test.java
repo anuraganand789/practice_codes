bc2a4ddedd8ef06b3a097d3f159a7adfeaf9b7cf
Anurag Anand <anuraganand789@hotmail.com>
2021-03-16 11:59:50 +0530

    writing code to understand java-modules

---
+++ java-codes/module-test/module.test/com/test/Test.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class Test{
+    public static void main(final String... args){
+        System.out.println("Test");
+    }
+}
---
+++ java-codes/module-test/module.test/module-info.java
@@ -0,0 +1,3 @@
+module module.test{
+    exports com.test;
+}
